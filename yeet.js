e7a43c3d0d9fbbe105ef22aa849d77fe069bac03
AramCodes <115132116+AramCodes@users.noreply.github.com>
2023-03-01 20:36:17 -0500

    Add files via upload

---
+++ yeet.js
@@ -0,0 +1,29 @@
+const isAlive = true
+
+alert(isAlive)
+
+let count = 0
+
+count = 2
+
+console.log(count)
+
+function addDivide(n1, n2, n3, n4) {
+    return (n1 + n2 + n3) / n4
+}
+
+addDivide(2,3,4,5)
+
+function yep(n1, n2) {
+    console.log(n1 ** n2)
+}
+
+yep(2,2)
+
+function (bool, string) {
+    if (bool === true) {                // b ? alert(str) : console.log(str)
+        alert(string)
+    }else if (b === false){
+        console.log(string)
+    }
+}
