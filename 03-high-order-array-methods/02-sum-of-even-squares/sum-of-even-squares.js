18e1581fee4db4a4fd1f149ab9b6f988d23a289b
Aram <johnnyvalcourt@yahoo.com>
2024-02-19 20:31:48 -0500

    "3 challenges done"

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,18 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(nums) {
+    const even = nums.filter((num) => num % 2 === 0);
+    const squares = even.map((num) => num * num);
+    return squares.reduce((sum, square) => sum + square, 0);
+}
 
 module.exports = sumOfEvenSquares;
+
+// chained version of above
+// function sumOfEvenSquares(numbers) {
+
+//     const evenSquares = numbers
+//         .filter((num) => num % 2 === 0)
+//         .map((num) => num ** 2)
+//         .reduce((sum, square) => sum + square, 0);
+
+//     return evenSquares;
+// }
