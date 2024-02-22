deb1c05b43ac11408ccee660a6cf9843d28265b4
Aram <johnnyvalcourt@yahoo.com>
2024-02-21 23:06:08 -0500

    "recursive test failed becaus of zero case not accounting for negative"

--- 04-recursion/02-unwinding/sum-up-to.js
+++ 04-recursion/02-unwinding/sum-up-to.js
@@ -1,9 +1,13 @@
 function sumUpTo(num) {
+    let sum = 0;
+
     if (num === 1) {
         return 1;
+    } else {
+        sum = num + sumUpTo(num - 1);
     }
 
-    return num + sumUpTo(num - 1);
+    return sum;
 }
 
 module.exports = sumUpTo;
