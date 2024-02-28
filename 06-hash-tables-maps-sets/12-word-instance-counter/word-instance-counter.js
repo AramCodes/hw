3a2c604013698a7e46f5c310648d1218c7b71f26
Aram <johnnyvalcourt@yahoo.com>
2024-02-28 15:57:19 -0500

    "maps hashtabels and sets challenges completed"

--- 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
+++ 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
@@ -1,5 +1,30 @@
-const HashTable = require('./HashTable');
+const HashTable = require("./HashTable");
 
-function wordInstanceCounter() {}
+function wordInstanceCounter(str, word) {
+    const formatedStr = str.toLowerCase().split(/\W+/);
+    const formatedWord = word.toLowerCase();
+    const frequencyTable = new HashTable();
+
+    let counter = 0;
+
+    for (const currentWord of formatedStr) {
+        if (currentWord === "") continue;
+
+        if (frequencyTable.has(currentWord)) {
+            frequencyTable.set(
+                currentWord,
+                frequencyTable.get(currentWord) + 1
+            );
+        } else {
+            frequencyTable.set(currentWord, 1);
+        }
+
+        if (currentWord === formatedWord) {
+            counter = frequencyTable.get(currentWord);
+        }
+    }
+
+    return counter;
+}
 
 module.exports = wordInstanceCounter;
--- 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
+++ 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
@@ -1,5 +1,19 @@
-const HashTable = require('./HashTable');
+const HashTable = require("./HashTable");
 
-function anagramGrouping() {}
+function anagramGrouping(strArr) {
+    const anagramGroups = new HashTable();
+
+    strArr.forEach((str) => {
+        const sortedChars = str.split("").sort().join("");
+
+        if (anagramGroups.has(sortedChars)) {
+            anagramGroups.get(sortedChars).push(str);
+        } else {
+            anagramGroups.set(sortedChars, [str]);
+        }
+    });
+
+    return anagramGroups.getValues();
+}
 
 module.exports = anagramGrouping;
