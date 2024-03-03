acc7ba69ce52df3e566603a211b6de628b69fa4e
Aram <johnnyvalcourt@yahoo.com>
2024-03-02 22:44:36 -0500

    "doubly linked list implementation done"

--- 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list-run.js
+++ 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list-run.js
@@ -13,4 +13,13 @@ list.prepend(50);
 list.insertAt(3, 250);
 list.insertAt(5, 350);
 
+console.log(list.get(3));
+console.log(list.get(3).data);
+
+list.remove(3);
+list.remove(4);
+
+console.log(list.contains("new"));
+console.log(list.contains(200));
+
 list.printAll();
