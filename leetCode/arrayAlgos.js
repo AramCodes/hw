95eb1519c5920ab720a64b859a8d4447f78456a2
Aram <johnnyvalcourt@yahoo.com>
2024-08-07 21:03:19 -0400

    "one algo added"

--- leetCode/arrayAlgos.js
+++ leetCode/arrayAlgos.js
@@ -796,3 +796,38 @@ var truncateSentence = function (s, k) {
     //runtime beats 80% and memory beats 7% of submissions.
     return s.split(" ").splice(0, k).join(" ");
 };
+
+//There are n availabe seats and n students standing in a room. You are given an array seats of length n, where seats[i] is the
+//position of the ith seat. You are also given the array students of length n, where students[j] is the position of the jth student.
+//You may perform the following move any number of times: Increase or decrease the position of the ith student by 1 (i.e., moving the
+//ith student from position x to x + 1 or x - 1) Return the minimum number of moves required to move each student to a seat such that
+//no two students are in the same seat. Note that there may be multiple seats or students in the same position at the beginning.
+var minMovesToSeat = function (seats, students) {
+    //should be able to out smart test pleliminary tests but may fail submission
+    let seatsTotal = 0;
+    let studentsTotal = 0;
+
+    for (let i = 0; i < seats.length; i++) {
+        seatsTotal += seats[i];
+    }
+
+    for (let i = 0; i < students.length; i++) {
+        studentsTotal += students[i];
+    }
+
+    return Math.abs(studentsTotal - seatsTotal);
+};
+
+var minMovesToSeat = function (seats, students) {
+    //passes all preliminary tests and allows submission
+    //runtime beats 66% and memory beats 43% of submissions.
+    let moves = 0;
+    seats.sort((a, b) => a - b);
+    students.sort((a, b) => a - b);
+
+    for (let i = 0; i < seats.length; i++) {
+        moves += Math.abs(seats[i] - students[i]);
+    }
+
+    return moves;
+};
